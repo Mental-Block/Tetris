bfd351dae33c3fa374510ef443a1481031c8fe70
Aaron <aaron@Aarons-MacBook-Air.local>
2020-02-20 23:03:56 -0500

    need to work on timer

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "rgb(0, 0, 0)";
 
+var pauseButton = document.getElementById("pause");
+var startButton = document.getElementById("start");
+
 //standard grid
 var row = 19; // + 1
 var column = 9; // + 1
+let x = 4;
+let y = 0;
+
 var grid = new Array();
 
 for (let i = 0; i <= row; i++) {
@@ -15,111 +21,125 @@ for (let i = 0; i <= row; i++) {
   }
 }
 
-//game pieces in memory
-const OBlock = [
-  [0, 0, 0, 0],
-  [0, 1, 1, 0],
-  [0, 1, 1, 0],
-  [0, 0, 0, 0]
-];
-
-const IBlock = [
-  [0, 1, 0, 0],
-  [0, 1, 0, 0],
-  [0, 1, 0, 0],
-  [0, 1, 0, 0]
-];
-
-const JBlock = [
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 1, 1, 1],
-  [0, 0, 0, 0]
-];
-
-const LBlock = [
-  [0, 0, 0, 0],
-  [0, 0, 1, 0],
-  [1, 1, 1, 0],
-  [0, 0, 0, 0]
-];
-
-const ZBlock = [
-  [0, 0, 0, 0],
-  [1, 1, 0, 0],
-  [0, 1, 1, 0],
-  [0, 0, 0, 0]
-];
-
-const SBlock = [
-  [0, 0, 0, 0],
-  [0, 1, 1, 0],
-  [1, 1, 0, 0],
-  [0, 0, 0, 0]
-];
-
-const TBlock = [
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [1, 1, 1, 0],
-  [0, 0, 0, 0]
-];
-
-//game pieces UI overlay
-
-//position in the array
-
-//game piece init
+// declare blocks in memory and DOM
 function blockPosAndPrint(a, b, c, d, e, f, g, h) {
   let block = ctx.fillRect(30 * a, 30 * b, 30 * c, 30 * d);
   let block2 = ctx.fillRect(30 * e, 30 * f, 30 * g, 30 * h);
   return block && block2;
 }
 
-//const ODisplayBlock = blockPosAndPrint(1, 1, 2, 2, 0, 0, 0, 0);
-//const IDisplayBlock = blockPosAndPrint(1, 1, 1, 4, 0, 0, 0, 0, 0);
-//const JDisplayBlock = blockPosAndPrint(1, 1, 1, 1, 1, 2, 3, 1);
-const LDisplayBlock = blockPosAndPrint(2, 0, 1, 1, 0, 1, 3, 1);
-//const ZDisplayBlock = blockPosAndPrint(0, 0, 2, 1, 1, 1, 2, 1);
-//const SDisplayBlock = blockPosAndPrint(1, 0, 2, 1, 0, 1, 2, 1);
-//const TDisplayBlock = blockPosAndPrint(0, 1, 3, 1, 1, 0, 1, 2);
-
-// get random piece
-let curBlock;
-let uiBlock;
-switch (1 /*Math.floor(Math.random() * 7)*/) {
-  case 0:
-    curBlock = ZBlock;
-    // uiBlock = ZDisplayBlock;
-    break;
-  case 1:
-    curBlock = OBlock;
-    //   uiBlock = ODisplayBlock;
-    break;
-  case 2:
-    curBlock = IBlock;
-    //  uiBlock = IDisplayBlock;
-    break;
-  case 3:
-    curBlock = JBlock;
-    //   uiBlock = JDisplayBlock;
-    break;
-  case 4:
-    curBlock = LBlock;
-    //   uiBlock = LDisplayBlock;
-    break;
-  case 5:
-    curBlock = SBlock;
-    //  uiBlock = SDisplayBlock;
-    break;
-  case 6:
-    curBlock = TBlock;
-    // uiBlock = TDisplayBlock;
-    break;
-  default:
-    console.log("error curBlock issue");
+class ODisplayBlock {
+  ODisplayBlock = blockPosAndPrint(x + 1, y, 1, 2, x, y, 1, 2);
+  OBlock = [
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0]
+  ];
+}
+class IDisplayBlock {
+  IDisplayBlock = blockPosAndPrint(x, y, 1, 4, 0, x, y, 0, 0);
+  IBlock = [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0]
+  ];
+}
+class JDisplayBlock {
+  JDisplayBlock = blockPosAndPrint(x, y, 1, 1, x, y + 1, 3, 1);
+  JBlock = [
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 1],
+    [0, 0, 0, 0]
+  ];
+}
+class LDisplayBlock {
+  LDisplayBlock = blockPosAndPrint(x, y, 1, 1, x, y + 1, 3, 1);
+  LBlock = [
+    [0, 0, 0, 0],
+    [0, 0, 1, 0],
+    [1, 1, 1, 0],
+    [0, 0, 0, 0]
+  ];
+}
+class ZDisplayBlock {
+  ZDisplayBlock = blockPosAndPrint(x + 1, y + 1, 2, 1, x, y, 2, 1);
+  ZBlock = [
+    [0, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0]
+  ];
+}
+class SDisplayBlock {
+  SDisplayBlock = blockPosAndPrint(x - 1, y + 1, 2, 1, x, y, 2, 1);
+  SBlock = [
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [1, 1, 0, 0],
+    [0, 0, 0, 0]
+  ];
+}
+class TDisplayBlock {
+  TDisplayBlock = blockPosAndPrint(x + 1, y, 1, 3, x, y + 1, 1, 1);
+  TBlock = [
+    [0, 0, 0, 0],
+    [0, 0, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0]
+  ];
 }
 
-//dropping the piece
+//PAUSE - OFF GAME LOOP ONTO TEMP LOOP
+var c = 0;
+var t;
+var pause = 0;
 
-// // rotate pieces
+function timedCount() {
+  document.getElementById("txt").value = c;
+  c = c + 1;
+  t = setTimeout(timedCount, 1000);
+}
+
+function startCount() {
+  if (!pause) {
+    pause = 1;
+    timedCount();
+  }
+}
+
+function stopCount() {
+  clearTimeout(t);
+  pause = 0;
+}
+
+function mainGameLoop() {
+  y++;
+  switch (0 /*Math.floor(Math.random() * 7)*/) {
+    case 0:
+      new ODisplayBlock();
+      break;
+    case 1:
+      new IDisplayBlock();
+      break;
+    case 2:
+      new LDisplayBlock();
+      break;
+    case 3:
+      new ZDisplayBlock();
+      break;
+    case 4:
+      new SDisplayBlock();
+      break;
+    case 5:
+      new TDisplayBlock();
+      break;
+    case 6:
+      new JDisplayBlock();
+      break;
+    default:
+      console.log("error curBlock issue");
+  }
+}
